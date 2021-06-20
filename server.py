1523f21ca34d54f00193dc51fea70afebf0f40f7
deivydaslipskis <deivydaslipskis@gmail.com>
2021-06-20 22:16:16 +0300

    add templates

--- server.py
+++ server.py
@@ -5,4 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!!!!!'
+
+
+@app.route('/blog')
+def blog():
+    return 'blogs'
+
+
+@app.route('/blog/2021/dogs')
+def blog2():
+    return 'dog'
