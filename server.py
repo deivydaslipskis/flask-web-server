f5c070397c05c4ef82461029c0afe593352a8bbf
deivydaslipskis <deivydaslipskis@gmail.com>
2021-06-21 16:25:16 +0300

    add html assets from mashup-template

--- server.py
+++ server.py
@@ -4,18 +4,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def my_home():
     return render_template('index.html')
-
-
-@app.route('/<username>/<int:post_id>')
-def show_user(username=None, post_id=None):
-    return render_template('index.html', name=username, post_id=post_id)
-
-
-@app.route('/blog')
-def blog():
-    return render_template('blogs.html')
 
 
 
