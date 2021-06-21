de4098157068756ca8a7205637dd39440958042a
deivydaslipskis <deivydaslipskis@gmail.com>
2021-06-21 22:44:20 +0300

    add route to submit form, add thankyou html

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -11,3 +11,17 @@ def my_home():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
